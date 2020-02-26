effa66f60e86c41399d62aeaab243da3cd979c86
ram5353 <ramkiran5353@gmail.com>
2020-02-26 16:27:51 +0530

    Got port through Environment Annotation

--- src/main/java/com/springcloud/microservices/currencyexchangeservice/CurrencyExchangeController.java
+++ src/main/java/com/springcloud/microservices/currencyexchangeservice/CurrencyExchangeController.java
@@ -1,18 +1,26 @@
 package com.springcloud.microservices.currencyexchangeservice;
 
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.math.BigDecimal;
+import java.util.Objects;
 
 @RestController
 public class CurrencyExchangeController {
 
+    @Autowired
+    private Environment environment;
+
     @GetMapping("/currency-exchange/from/{from}/to/{to}")
     public ExchangeValue retrieveExchageValue(@PathVariable String from, @PathVariable String to ) {
-        return new ExchangeValue(1000L, from, to, BigDecimal.valueOf(65));
+        ExchangeValue exchangeValue = new ExchangeValue(1000L, from, to, BigDecimal.valueOf(65));
+        exchangeValue.setPort(Integer.parseInt(Objects.requireNonNull(environment.getProperty("local.server.port"))));
+        return exchangeValue;
     }
 
 }
