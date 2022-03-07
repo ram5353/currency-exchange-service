080a9cd4612a82b4459d475ce43a8bd7f6e4198c
ram5353 <ramkiran5353@gmail.com>
2022-03-06 20:44:55 -0500

    Initial commit

---
+++ src/main/java/com/microservice/currencyexchangeservice/CurrencyExchange.java
@@ -0,0 +1,40 @@
+package com.microservice.currencyexchangeservice;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import java.math.BigDecimal;
+
+@Entity
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class CurrencyExchange {
+
+    public CurrencyExchange(Long id, String from, String to, BigDecimal conversionMultiple) {
+        super();
+        this.id = id;
+        this.from = from;
+        this.to = to;
+        this.conversionMultiple = conversionMultiple;
+    }
+
+    @Id
+    private Long id;
+
+    @Column(name = "currency_from")
+    private String from;
+
+    @Column(name = "currency_to")
+    private String to;
+
+    private BigDecimal conversionMultiple;
+    private String environment;
+}
